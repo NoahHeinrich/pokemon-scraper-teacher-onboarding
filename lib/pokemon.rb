6c151ca5d9763c3c7353a08885b649fed6db9e55
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-20 20:48:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_reader :id, :db
   attr_accessor :name, :type, :hp
-  def initialize(id:,name:,type:,db:, hp: nil)
+  def initialize(id: nil,name:nil,type:nil,db: nil, hp: nil)
     @id = id
     @name = name
     @type = type
@@ -11,12 +11,12 @@ class Pokemon
   
   def self.save(name,type,db, hp=60)
     sql ="
-      INSERT INTO pokemon (name, type)
-      VALUES(?, ?)
+      INSERT INTO pokemon (name, type, hp)
+      VALUES(?, ?, ?)
     "
     db.execute(sql, name, type)
     result =db.execute("SELECT last_insert_rowid() FROM pokemon")
-    Pokemon.new(id: result[0][0],name: name,type: type, db: db, hp: hp)
+    Pokemon.new(id: result[0][0],name: name,type: type)
   end
   
   def self.find(id,db)
@@ -27,9 +27,13 @@ class Pokemon
   
   def alter_hp(new_hp,db)
     poke_data = Pokemon.find(self.id,self.db)
-    
-    
+    poke_data.hp = new_hp
+    poke_data.update
   end
   
+  def update
+    sql ="UPDATE pokemon SET name = ?, type = ?, hp = ? WHERE id = ?"
+    db.execute(sql, self.name,self.type,self.hp,self.id)
+  end
      
 end
